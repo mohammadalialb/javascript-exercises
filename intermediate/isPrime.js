06402983f6c73f55112796d4f156d3d401404496
Mohammadali alb <m.ali.alb.7957a@gmail.com>
2025-02-14 10:29:36 +0330

    solved isPrime

--- intermediate/isPrime.js
+++ intermediate/isPrime.js
@@ -12,4 +12,12 @@
 
 // Write your solution here
 
+function isPrime(num) {
+    if (num < 2) return false;
+        for(let i = 2; i <= Math.sqrt(num);i++) {
+            if(num % i ===0) return false
+        }
+        return true
+}
+
 module.exports = isPrime;
