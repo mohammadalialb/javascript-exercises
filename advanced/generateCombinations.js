ebefa83f838f9b6bbacb23d335505a10462e5134
Mohammadali alb <m.ali.alb.7957a@gmail.com>
2025-02-14 11:01:25 +0330

    solved generateCombinations

--- advanced/generateCombinations.js
+++ advanced/generateCombinations.js
@@ -11,4 +11,28 @@
  * [ [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3] ].
  */
 
+function generateCombinations(nums) {
+    const result = [];
+    const n = nums.length;
+
+
+    const backtrack = (start, current, length) => {
+        if (current.length === length) {
+            result.push([...current]);
+            return;
+        }
+        for (let i = start; i < n; i++) {
+            current.push(nums[i]);
+            backtrack(i + 1, current, length);
+            current.pop();
+        }
+    };
+
+    for (let len = 1; len <= n; len++) {
+        backtrack(0, [], len);
+    }
+
+    return result;
+}
+
 module.exports = generateCombinations;
