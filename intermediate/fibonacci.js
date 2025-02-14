ea96164c186cc2fdc31d7633a6b528ff3cf74ba8
Mohammadali alb <m.ali.alb.7957a@gmail.com>
2025-02-14 10:30:50 +0330

    solved fibonacci

--- intermediate/fibonacci.js
+++ intermediate/fibonacci.js
@@ -12,4 +12,17 @@
 
 // Write your solution here
 
+function fibonacci(n) {
+    if(n<=0) return[]
+    if(n===1) return[0]
+
+    let sequence = [0,1]
+
+    for(let i = 2 ; i<n ; i++){
+        sequence.push(sequence[i-1] + sequence[i-2])
+    }
+    return sequence
+}
+
+
 module.exports = fibonacci;
