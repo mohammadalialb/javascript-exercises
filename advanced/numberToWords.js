31233686f21a0e65187a79c02f6de23a59a5ab9e
mohammadalialb <m.ali.alb.7957a@gmail.com>
2025-02-14 16:34:27 +0330

    Update numberToWords.js

--- advanced/numberToWords.js
+++ advanced/numberToWords.js
@@ -11,5 +11,30 @@
  */
 
 // Write your solution here
+const numberToWords = num => {
+    if (num === 0) return "zero";
 
+    const belowTwenty = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+        "seventeen", "eighteen", "nineteen"];
+
+    const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const thousands = ["", "thousand", "million", "billion"];
+
+    const helper = n =>
+        n === 0 ? "" :
+        n < 20 ? belowTwenty[n] + " " :
+        n < 100 ? tens[Math.floor(n / 10)] + " " + helper(n % 10) :
+        belowTwenty[Math.floor(n / 100)] + " hundred " + helper(n % 100);
+
+    let result = "", i = 0;
+    
+    while (num > 0) {
+        if (num % 1000) result = helper(num % 1000) + thousands[i] + " " + result;
+        num = Math.floor(num / 1000);
+        i++;
+    }
+
+    return result.trim();
+};
 module.exports = numberToWords;
